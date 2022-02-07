b24c79d1495e76d26ab733bd70e5a249a2b6529f
lfjnascimento <lfjnascimento@gmail.com>
2022-02-06 21:56:23 -0400

    Fiz max size code error

--- contracts/BCoinProject.sol
+++ contracts/BCoinProject.sol
@@ -37,7 +37,7 @@ struct Project{
 contract BCoinProject {
 
   // Armazena o ID a ser utilizado na criacao do proximo projeto
-  uint256 private _next_ID = 0;
+  uint256 public _next_ID = 0;
   
   // Armazena a quantidade de projetos que estao no estado created
   uint32 public qdtProjectsInCreatedStatus = 0;
@@ -46,16 +46,16 @@ contract BCoinProject {
   mapping(uint256 => Project) projects;
   
   // Mapea endereco para Id dos projetos onde o endereco tem papel de executor
-  mapping(address => uint256[]) executorOf;
+  mapping(address => uint256[]) public executorOf;
   
   // Mapea endereco para Id dos projetos onde o endereco tem papel de autoridade
-  mapping(address => uint256[]) authorityOf;
+  mapping(address => uint256[]) public authorityOf;
 
 // Mapea endereco para Id dos projetos onde o endereco tem papel de autoridade
-  mapping(address => uint256[]) proponentOf;
+  mapping(address => uint256[]) public proponentOf;
 
   modifier onlyProjectAuthority(uint256 _project_ID){
-    require(msg.sender == projects[_project_ID].authority, "only_project_authority");
+    require(msg.sender == projects[_project_ID].authority, "NOTauthority");
     _;
   }
 
@@ -95,7 +95,7 @@ contract BCoinProject {
   * - O projeto tem que estar no status created.
   */
   function toSign(uint256 _ID) public {
-    require(projects[_ID].status == Status.created, "unsignable_contract");
+    require(projects[_ID].status == Status.created, "NOTcreated");
 
     projects[_ID].executor = msg.sender;
     projects[_ID].status = Status.in_execution;
@@ -110,7 +110,7 @@ contract BCoinProject {
   * - O projeto tem que estar no status in_execution.
   */
   function setPercentDone(uint256 _project_ID, uint8 _percentage) public onlyProjectAuthority(_project_ID){
-    require(projects[_project_ID].status == Status.in_execution, "project_not_in_execution");
+    require(projects[_project_ID].status == Status.in_execution, "not_in_execution");
 
     if (_percentage < 100)
       projects[_project_ID].percent_done = _percentage;
@@ -126,8 +126,8 @@ contract BCoinProject {
   * - Deve ser executada apenas pelo executor do projeto.
   */
   function addFiles(uint256 _project_ID, string[] memory _files_cids, string[] memory _files_mime_types) public{
-    require(projects[_project_ID].status == Status.in_execution, "project_not_in_execution");
-    require(projects[_project_ID].executor == msg.sender, "Only_project_executor");
+    require(projects[_project_ID].status == Status.in_execution, "not_in_execution");
+    require(projects[_project_ID].executor == msg.sender, "NOTexecutor");
 
     Media memory file;
     for (uint i=0; i < _files_cids.length; i++) {
@@ -145,7 +145,7 @@ contract BCoinProject {
   * - Deve ser executada apenas pela autoridade do projeto.
   */
   function addComment(uint256 _project_ID, string memory _comment) onlyProjectAuthority(_project_ID) public {
-    require(projects[_project_ID].status == Status.in_execution, "project_not_in_execution");
+    require(projects[_project_ID].status == Status.in_execution, "not_in_execution");
 
     projects[_project_ID].comment = _comment;
   }
@@ -177,7 +177,7 @@ contract BCoinProject {
     projects[_project_ID].balance = 0;
     
     (bool success, ) = executor.call{value: balance}("");
-    require(success, "tranfer_failed");    
+    require(success, "");    
   }  
 
   /*
@@ -187,7 +187,7 @@ contract BCoinProject {
   * - O projeto deve ter sido criado.
   */
   function getProject(uint256 _ID) public view returns(Project memory){
-    require(projects[_ID].status != Status.not_created, "project_not_exists");
+    require(projects[_ID].status != Status.not_created, "NOTexists");
     return projects[_ID];
   }
   
@@ -205,171 +205,49 @@ contract BCoinProject {
     return _projects;
   }
   
-  function getExecutorOf(address _executor) public view returns(uint256[] memory){
-    return executorOf[_executor];
-  }
-  
-  function getAuthorityOf(address _authority) public view returns(uint256[] memory){
-    return authorityOf[_authority];
-  }
 
-  /*
-  * Retorna todos os projetos em execucao onde o endereco _executor tem papel de executor
-  */
-  function getExecutorProjectsInExecution(address _executor) public view returns(Project[] memory){
-    uint qtdProjects = executorOf[_executor].length;
-    Project[] memory _projects = new Project[](qtdProjects);
-    
-    uint256 qtdProjectsInExecution = 0;
-    for(uint256 i=0; i < qtdProjects; i++){
-      uint256 projectID = executorOf[_executor][i];
-      
-        if (projects[projectID].status == Status.in_execution){
-          _projects[qtdProjectsInExecution] = projects[projectID];
-          qtdProjectsInExecution++;
-        }
-    }
-    
-    Project[] memory _result = new Project[](qtdProjectsInExecution);
-    for(uint256 i=0; i < qtdProjectsInExecution; i++){
-      _result[i] = _projects[i];
+  function getProjectsOfInExecution(address _addr) public view returns(Project[] memory){
+    uint256 _qtdProjects = 0;
+
+    for(uint256 ID=0; ID < _next_ID; ID++){
+      if (projects[ID].status == Status.in_execution && (projects[ID].authority == _addr || projects[ID].proponent == _addr || projects[ID].executor == _addr)){
+        _qtdProjects++;
+      }
     }
 
-    return  _result;
+    Project[] memory _projectsOf = new Project[](_qtdProjects);
+    uint256 _projectsOfID = 0;
+
+    for(uint256 ID=0; ID < _next_ID; ID++){
+      if (projects[ID].status == Status.in_execution && (projects[ID].authority == _addr || projects[ID].proponent == _addr || projects[ID].executor == _addr)){
+        _projectsOf[_projectsOfID] = projects[ID];
+        _projectsOfID++;
+      }
+    }
+
+    return _projectsOf;
   }
 
+   function getProjectsOfFinished(address _addr) public view returns(Project[] memory){
+    uint256 _qtdProjects = 0;
 
-  /*
-  * Retorna todos os projetos finalizados onde o endereco _executor tem papel de executor
-  */
-  function getExecutorProjectsFinished(address _executor) public view returns(Project[] memory){
-    uint qtdProjects = executorOf[_executor].length;
-    Project[] memory _projects = new Project[](qtdProjects);
-    
-    uint256 qtdProjectsFinished = 0;
-    for(uint256 i=0; i < qtdProjects; i++){
-      uint256 projectID = executorOf[_executor][i];
-      
-        if (projects[projectID].status == Status.finished){
-          _projects[qtdProjectsFinished] = projects[projectID];
-          qtdProjectsFinished++;
-        }
-    }
-    
-    Project[] memory _result = new Project[](qtdProjectsFinished);
-    for(uint256 i=0; i < qtdProjectsFinished; i++){
-      _result[i] = _projects[i];
+    for(uint256 ID=0; ID < _next_ID; ID++){
+      if (projects[ID].status == Status.finished && (projects[ID].authority == _addr || projects[ID].proponent == _addr || projects[ID].executor == _addr)){
+        _qtdProjects++;
+      }
     }
 
-    return  _result;
-  }
-  
-  
-  /*
-  * Retorna todos os projetos em execucao onde o endereco _authority tem papel de autoridade
-  */
-  function getAuthorityProjectsInExecution(address _proponent) public view returns(Project[] memory){
-    uint qtdProjects = proponentOf[_proponent].length;
-    Project[] memory _projects = new Project[](qtdProjects);
-     
-    uint256 qtdProjectsInExecution = 0;
-    for(uint256 i=0; i < qtdProjects; i++){
-      uint256 projectID = proponentOf[_proponent][i];
-      
-        if (projects[projectID].status == Status.in_execution){
-          _projects[qtdProjectsInExecution] = projects[projectID];
-          qtdProjectsInExecution++;
-        }
-    }
-    
-    Project[] memory _result = new Project[](qtdProjectsInExecution);
-    for(uint256 i=0; i < qtdProjectsInExecution; i++){
-      _result[i] = _projects[i];
+    Project[] memory _projectsOf = new Project[](_qtdProjects);
+    uint256 _projectsOfID = 0;
+
+    for(uint256 ID=0; ID < _next_ID; ID++){
+      if (projects[ID].status == Status.finished && (projects[ID].authority == _addr || projects[ID].proponent == _addr || projects[ID].executor == _addr)){
+        _projectsOf[_projectsOfID] = projects[ID];
+        _projectsOfID++;
+      }
     }
 
-    return  _result;
-  }
-
-  /*
-  * Retorna todos os projetos finalizados onde o endereco _authority tem papel de autoridade
-  */
-  function getAuthorityProjectsFinished(address _authority) public view returns(Project[] memory){
-    uint qtdProjects = authorityOf[_authority].length;
-    Project[] memory _projects = new Project[](qtdProjects);
-     
-    uint256 qtdProjectsFinished = 0;
-    for(uint256 i=0; i < qtdProjects; i++){
-      uint256 projectID = authorityOf[_authority][i];
-      
-        if (projects[projectID].status == Status.finished){
-          _projects[qtdProjectsFinished] = projects[projectID];
-          qtdProjectsFinished++;
-        }
-    }
-    
-    Project[] memory _result = new Project[](qtdProjectsFinished);
-    for(uint256 i=0; i < qtdProjectsFinished; i++){
-      _result[i] = _projects[i];
-    }
-
-    return  _result;
-  }
-  
-  /*
-  * Retorna todos os projetos em execucao onde o endereco _proponent tem papel de proponente
-  */
-  function getProponentProjectsInExecution(address _authority) public view returns(Project[] memory){
-    uint qtdProjects = authorityOf[_authority].length;
-    Project[] memory _projects = new Project[](qtdProjects);
-     
-    uint256 qtdProjectsInExecution = 0;
-    for(uint256 i=0; i < qtdProjects; i++){
-      uint256 projectID = authorityOf[_authority][i];
-      
-        if (projects[projectID].status == Status.in_execution){
-          _projects[qtdProjectsInExecution] = projects[projectID];
-          qtdProjectsInExecution++;
-        }
-    }
-    
-    Project[] memory _result = new Project[](qtdProjectsInExecution);
-    for(uint256 i=0; i < qtdProjectsInExecution; i++){
-      _result[i] = _projects[i];
-    }
-
-    return  _result;
-  }
-
-  /*
-  * Retorna todos os projetos finalizados onde o endereco _proponent tem papel de proponente
-  */
-  function getProponentProjectsFinished(address _proponent) public view returns(Project[] memory){
-    uint qtdProjects = proponentOf[_proponent].length;
-    Project[] memory _projects = new Project[](qtdProjects);
-     
-    uint256 qtdProjectsFinished = 0;
-    for(uint256 i=0; i < qtdProjects; i++){
-      uint256 projectID = proponentOf[_proponent][i];
-      
-        if (projects[projectID].status == Status.finished){
-          _projects[qtdProjectsFinished] = projects[projectID];
-          qtdProjectsFinished++;
-        }
-    }
-    
-    Project[] memory _result = new Project[](qtdProjectsFinished);
-    for(uint256 i=0; i < qtdProjectsFinished; i++){
-      _result[i] = _projects[i];
-    }
-
-    return  _result;
+    return _projectsOf;
   }
  
-  function getContractBalance() public view returns (uint256 ){
-    return address(this).balance;
-  }
-
-  function getLastID() public view returns (uint256 ){
-    return _next_ID - 1;
-  }
 }
